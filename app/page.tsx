9f90f70b253f6bd89817b83ca7d5d5a7b9e3b516
Satyam Sagar <satyamsagar30@gmail.com>
2026-06-06 05:59:29 +0530

    added service section

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,7 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <h2 className="text-[32px] leading-150 font-medium text-center px-10">
+              <h3 className="text-[32px] leading-150 font-medium text-center px-10">
                 At Mindora, we believe mental wellness is a vital part of a
                 fulfilling life. Our licensed therapists provide{" "}
                 <span className="text-[#51575C]">
@@ -150,7 +150,7 @@ export default function Home() {
                   anxiety, trauma, or life transitions all in a safe,
                   confidential space.
                 </span>
-              </h2>
+              </h3>
               <div className="flex items-center justify-center">
                 <div className="bg-primary py-3 px-6 rounded-full text-white leading-120 text-sm font-medium flex items-center gap-1.5">
                   More About Us
@@ -205,6 +205,112 @@ export default function Home() {
                       Clients
                       <br />
                       Satisfaction
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-19 px-3">
+        <div className="bg-[#E3EEEE] rounded-2xl py-38">
+          <div className="container mx-auto">
+            <div className="flex items-stretch gap-20">
+              <div className="w-1/2 space-y-8">
+                <div className="space-y-6">
+                  <div className="flex items-center gap-2">
+                    <div className="w-1.5 h-1.5 bg-primary rounded-full" />
+                    <div className="text-sm leading-150 tracking-[8%] font-medium uppercase">
+                      Our Services
+                    </div>
+                  </div>
+                  <h2 className="text-[48px] font-semibold leading-120 tracking-[-3%]">
+                    Professional Mental Health{" "}
+                    <span className="font-playfair-display italic">
+                      Support Tailored to You
+                    </span>
+                  </h2>
+                  <p className="text-base leading-150 text-[#51575C]">
+                    We provide a wide range of therapy and mental wellness
+                    services to support your unique you're facing anxiety,
+                    depression, trauma, or personal growth. Our team ensures
+                    each session is safe, confidential, and client-centered.
+                  </p>
+                </div>
+                <div className="flex items-center">
+                  <div className="bg-primary py-3 px-6 rounded-full text-white leading-120 text-sm font-medium flex items-center gap-1.5">
+                    View All Services
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="text-white"
+                    >
+                      <path d="M5 12h14" />
+                      <path d="m12 5 7 7-7 7" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/2 relative">
+                <div className="absolute top-0 left-0 z-10 w-full h-full">
+                  <div className="bg-white rounded-xl border border-[#EEF2F5] h-full"></div>
+                </div>
+                <div className="absolute top-6 left-0 z-20 w-full h-[calc(100%-1.5rem)]">
+                  <div className="bg-white rounded-xl border border-[#EEF2F5] h-full"></div>
+                </div>
+                <div className="absolute bottom-0 left-0 z-30">
+                  <div className="bg-white rounded-xl border border-[#EEF2F5] p-6">
+                    <div className="space-y-8">
+                      <div className="flex items-center justify-between">
+                        <div className="w-16 h-16 rounded-full bg-gray-300" />
+                        <div className="space-y-0">
+                          <div className="text-sm leading-150 tracking-[8%] text-[#51575C] font-medium uppercase">
+                            Start From
+                          </div>
+                          <div className="flex items-end gap-1">
+                            <div className="font-semibold text-[22px] leading-120">
+                              $185
+                            </div>
+                            <div className="font-playfair-display text-sm italic leading-120">
+                              /Session
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="space-y-4">
+                        <div className="space-y-3">
+                          <div className="font-semibold text-[22px] text-[#0F7676] leading-120">
+                            Family{" "}
+                            <span className="font-playfair-display italic">
+                              Therapy
+                            </span>
+                          </div>
+                          <p className="text-base leading-150 text-[#51575C]">
+                            Build healthier family dynamics and address
+                            collective issues mental through collaborative,
+                            guided sessions.
+                          </p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                          <div className="text-sm leading-150 text-[#51575C] px-3 py-1.5 rounded-full border border-[#EEF2F5]">
+                            Group Healing
+                          </div>
+                          <div className="text-sm leading-150 text-[#51575C] px-3 py-1.5 rounded-full border border-[#EEF2F5]">
+                            Behavioral Guidance
+                          </div>
+                          <div className="text-sm leading-150 text-[#51575C] px-3 py-1.5 rounded-full border border-[#EEF2F5]">
+                            Stronger Bonds
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
